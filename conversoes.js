aa5c30267b47d616b2b52c2a805adcdc1bc2d376
Joel Adriano <joel.adster@gmail.com>
2022-05-10 20:36:54 -0300

    conversões e truthy-false

---
+++ conversoes.js
@@ -0,0 +1,5 @@
+//conversão implícita
+const number = 456
+const numberString = '456'
+console.log(number === numberString)
+console.log(number + Number(numberString))
---
+++ truthy-e-falsy.js
@@ -0,0 +1,17 @@
+//truthy ou falsy
+
+console.log(0 == false)
+console.log('' == false)
+console.log(1 == true)
+
+let minhaVar
+let varNull = null
+console.log(minhaVar)
+console.log(varNull)
+
+let numero = 3
+let texto = 'Alura'
+console.log(typeof numero)
+console.log(typeof texto)
+console.log(typeof minhaVar)
+console.log(typeof varNull)

8914adac5f0712f324fa8bd67524d4f594aaab01
Joel Adriano <joel.adster@gmail.com>
2022-05-10 17:48:51 -0300

    Aula 02, sobre const, let e var

--- type-boolean.js
+++ type-boolean.js
@@ -6,3 +6,8 @@ console.log(number1 === number2)
 const texto1 = 'Alura'
 const texto2 = 'alura'
 console.log(texto1 === texto2)
+
+const nome = 'Lara'
+const sobre = 'Ayume'
+const completo = nome + sobre
+console.log(completo)
